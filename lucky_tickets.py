67d82bf4c83420c4a5112026d4f7a62c307643d7
MatekaytisDK <matekaitis@yandex.ru>
2023-08-29 23:54:32 +0300

    Завершение кода со счастливыми билетами

--- lucky_tickets.py
+++ lucky_tickets.py
@@ -1,31 +1,64 @@
 def check_input(num):
     try:
         num = int(num)
-        if num % 2 == 0:
+        if num == 4 or num == 6:
             return num
         else:
-            print('Not an even integer')
+            print('Ошибка! Введено число ни 4, ни 6')
             return None 
     except:
-        print('Not a integer')
+        print('Ошибка! Введено не число')
         return None
         
 def search_lucky(num):
-    all_tickets = range(10**num)
-    last_ticket = (all_tickets[::-1])[0]
-    return last_ticket
-#        str_lt = str(last_ticket)
-#       for i in range(n):
-#          ticket =str(i)
-#           if len(ticket) < len(str_lt):
-#                ticke(t = '0'*(len(str_lt)-len(ticket))+str(i)
-#               print(ticket)
-#            print(sum1,sum2)
+    all_tickets = []
+    lucky_numbers = []
+    all_numbers = range(10**num)
+    last_ticket = str((all_numbers[::-1])[0])
+    len_numbers = len(last_ticket)
+    
+    for i in all_numbers:
+        number = str(i)
+        if len(number) < len_numbers:
+            add_zero = '0' * (len_numbers - len(number))
+            ticket_number = add_zero + number
+            all_tickets.append(ticket_number)
+        else:
+            all_tickets.append(number)
+            
+    first_num = range(int(len_numbers / 2))
+    last_num = range(int(len_numbers))[int(len_numbers / 2):int(len_numbers)]
+    
+    for i in all_tickets:
+        sum1 = 0
+        sum2 = 0
+        for x1 in first_num:
+            sum1 += int(i[x1])
+        for x2 in last_num:
+            sum2 += int(i[x2])
+        if sum1 == sum2:
+            lucky_numbers.append(i)
+    return lucky_numbers
+
  
-#try:
-num = input('How many digits are on the ticket? (There must be an evem number) ')	
-result = check_input(num)
-if result:
-    print(search_lucky(result)) 
-#except:
-#	print('Application error')
+try:
+    while True:
+        num = input('Сколько цифр в билете? (Введите число 4 или 6)\n')
+        result = check_input(num)
+        if result:	
+            all_lucky = search_lucky(result)
+            print('Общее число счастливых билетов равно ' + str(len(all_lucky)))
+            while True:
+                answer = input('Показать их? Введите "да", если ответ - да, или "нет", если ответ нет.\n')
+                if answer == 'да':
+                    list_ticket = ';\n'.join(all_lucky)
+                    print(list_ticket)
+                    break
+                if answer == 'нет':
+                    print('Программа завершена.')
+                    break
+                else:
+                    print('Ошибка! Повторите ввод!')
+            break
+except:
+    print('Application error')
